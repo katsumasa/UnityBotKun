579bcf93366eef7b1e7357ffb1ede1b012f2350d
katsumasa <katsumasa@unity3d.com>
2021-04-19 19:00:26 +0900

    - フォーカスが外れている場合もProcessが実行されるように変更

--- Scripts/StandaloneInputModuleOverride.cs
+++ Scripts/StandaloneInputModuleOverride.cs
@@ -47,20 +47,74 @@ namespace Utj
         }
 
 
-        public override void UpdateModule()
-        {            
-            base.UpdateModule();            
-        }
-
 
         protected override void OnDestroy()
         {
-            if(instance == this)
-            {
+            base.OnDestroy();
+            if (instance == this)
+            {                
                 instance = null;
             }
         }
 
-        
+        public override void Process()
+        {
+#if UNITY_EDITOR
+            // eventSystem.isFocusedはvoid OnApplicationFocus(bool hasFocus)によって設定されるが、
+            // UnityEditorの場合、GameViewからフォーカスが外れると、
+            // フォーカスが外れたと認定される為、Processが実行されなくなるが、
+            // それでは困る為、UnityEditor上で実行する場合、フォーカスを無視するようにProcessの処理を変更
+
+            //if (!eventSystem.isFocused && ShouldIgnoreEventsOnNoFocus())
+            //    return;
+
+
+            bool usedEvent = SendUpdateEventToSelectedObject();
+
+            // case 1004066 - touch / mouse events should be processed before navigation events in case
+            // they change the current selected gameobject and the submit button is a touch / mouse button.
+
+            // touch needs to take precedence because of the mouse emulation layer
+            if (!ProcessTouchEvents() && input.mousePresent)
+                ProcessMouseEvent();
+
+            if (eventSystem.sendNavigationEvents)
+            {
+                if (!usedEvent)
+                    usedEvent |= SendMoveEventToSelectedObject();
+
+                if (!usedEvent)
+                    SendSubmitEventToSelectedObject();
+            }
+#else
+            base.Process();
+#endif
+        }
+
+        private bool ProcessTouchEvents()
+        {
+            for (int i = 0; i < input.touchCount; ++i)
+            {
+                Touch touch = input.GetTouch(i);
+
+                if (touch.type == TouchType.Indirect)
+                    continue;
+
+                bool released;
+                bool pressed;
+                var pointer = GetTouchPointerEventData(touch, out pressed, out released);
+
+                ProcessTouchPress(pointer, pressed, released);
+
+                if (!released)
+                {
+                    ProcessMove(pointer);
+                    ProcessDrag(pointer);
+                }
+                else
+                    RemovePointerData(pointer);
+            }
+            return input.touchCount > 0;
+        }
     }
 }
